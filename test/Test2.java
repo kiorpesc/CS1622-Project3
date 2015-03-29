46ad0141e04bb056437b60117760023fd7937f45
Sheridan Zivanovich <sdzivanovich@gmail.com>
2015-03-29 01:41:21 +0100

    Add test MiniJava source files

---
+++ test/Test2.java
@@ -0,0 +1,22 @@
+class Test2
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new Foo().foo());
+    }
+}
+
+class Foo
+{
+    public int foo()
+    {
+        int x;
+        int x;
+        return 1;
+    }
+}
+
+class Foo
+{
+    
+}
