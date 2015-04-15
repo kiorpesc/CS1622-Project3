1276ad6d6d40383c2fdad0d9191746feaa882ce3
Sheridan Zivanovich <sdzivanovich@gmail.com>
2015-04-15 08:09:05 +0100

    Add arrays to optimization test file

--- test/OptimizationTest.java
+++ test/OptimizationTest.java
@@ -12,6 +12,8 @@ class Derp
     {
         int x;
         boolean b;
+        int i;
+        int[] array;
 
         x = 1;
         b = false && true;
@@ -25,6 +27,15 @@ class Derp
             x = 3 + 2 + 3 * 2;
         }
         System.out.println(x);
+
+        i = 0;
+        array = new int[15 + 5];
+        while (i < 5)
+        {
+            array[2 + 2] = 20;
+            System.out.println(array[2 * 1 + 2]);
+            i = i + 1;
+        }
 
         return x;
     }
