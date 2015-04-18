995f0b6e743bc0e41d2c649cb7b4799a0d1a8f6e
Charles Kiorpes <ckiorpes@gmail.com>
2015-04-18 17:30:52 -0400

    Added crazy coalesce test

---
+++ test/CoalesceTest.java
@@ -0,0 +1,64 @@
+class Test {
+    public static void main(String[] args) {
+        System.out.println(new Test2().Start(9)); // should print 196418
+    }
+}
+class Test2 {
+  int z;
+    public int Start(int y) {
+      int a;
+      int b;
+      int c;
+      int d;
+      int e;
+      int f;
+      int g;
+      int h;
+      int i;
+      int j;
+      int k;
+      int l;
+      int m;
+      int n;
+      int o;
+      int p;
+      int q;
+      int r;
+      int s;
+      int t;
+      int u;
+      int v;
+      int w;
+      int x;
+      int z;
+
+      a = 1;
+      b = a + 1;
+      c = b + a;
+      d = c + b;
+      e = d + c;
+      f = e + d;
+      g = f + e;
+      h = g + f;
+      i = h + g;
+      j = i + h;
+      k = j + i;
+      l = k + j;
+      m = l + k;
+      n = m + l;
+      o = n + m;
+      p = o + n;
+      q = p + o;
+      r = q + p;
+      s = r + q;
+      t = s + r;
+      u = t + s;
+      v = u + t;
+      w = v + u;
+      x = w + v;
+      y = x + w;
+      z = y + x;
+
+      return z;
+    }
+}
