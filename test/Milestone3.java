eed53a397c517e1a989d8cde11a0028fe30f9593
Sheridan Zivanovich <sdzivanovich@gmail.com>
2015-04-19 22:37:17 +0100

    Fix milestone verification script

--- test/Milestone3.java
+++ test/Milestone3.java
@@ -1,10 +1,23 @@
 class Test {
     public static void main(String[] args) {
-        System.out.println(new Test2().Start(9));
+        System.out.println(new Test2().First(9));
     }
 }
+
 class Test2 {
-    public int Start(int y) {
+
+    int z;
+
+    public int First(int y) {
+        z = y;
+        y = this.Second(y, z, 4);
+        z = y + 3;
         return y;
+    }
+
+    public int Second(int y, int z, int a) {
+      int f;
+      f = y + z + a;
+      return f;
     }
 }
